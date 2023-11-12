e033e7e69f1f00e0414aff21a87da678ef823472
[Ayman Shiham] <sharktank38000@gmail.com>
2023-11-11 21:31:25 -0500

    added pages folder and installed react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import About from './components/About'
+import Contact from './components/'
+import Header from './components/'
+import Portfolio from './components/'
+import Resume from './components/'
+import Footer from './components/'
+
 import './App.css'
 
 function App() {
